dafcdf97f74ebc6234e22840d2e539767f89c446
Kenneth Cramer <i.cramer@live.com>
2019-02-06 12:27:13 -0800

    Updated Testing

--- index.test.js
+++ index.test.js
@@ -1,7 +1,52 @@
 const add = require('./index');
 
-test('test adding function', () => {
-  expect(add(1, 2)).toBe(3);
-  expect(add(5,0)).toBe(5);
-  expect(add(3)).toBe(3);
+test('Add Function as Expected', () =>
+{
+	expect(add(1, 2)).toBe(3); // Function as is expected
+});
+
+test('Add Function With a 0', () =>
+{
+	expect(add(3,0)).toBe(3);
+});
+
+test('Add Function With Decimals', () =>
+{
+	expect(add(2.7, 1.4)).toBeCloseTo(4.1);
+});
+
+test('Add Function With Negatives', () =>
+{
+	expect(add(-5, 12)).toBe(7);
+});
+
+test('Add Function With One Argument \n Result: Added to Default (0)', () =>
+{
+	expect(add(4)).toBe(4);
+});
+
+test('Add Function With No Arguments \n Result: Two Defaults (0)', () =>
+{
+	expect(add()).toBe(0);
+});
+
+test('Add Function With Too Many Arguments \n Result: Only First 2 Arguments Used', () =>
+{
+	expect(add(5,3,1)).toBe(8);
+});
+
+test('Add Function With A String \n Result: NaN', () =>
+{
+	expect(add('a string', 2)).toBe(NaN);
+});
+
+test('Add Function With A Bool \n Result: NaN', () =>
+{
+	expect(add(true, 5)).toBe(NaN);
+});
+
+test('Add Function With An Object \n Result: NaN', () =>
+{
+	var car = {make:"Tesla", model:"Roadster", year:2020, miles:2745, founderSeries:false, value:200000}
+	expect(add(car, 10)).toBe(NaN);
 });
